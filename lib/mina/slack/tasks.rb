9a8bdc2af1c5373ca9d686d1a78db1688323f6d6
Roman Osidach <hard.prophecy@gmail.com>
2018-03-28 18:26:35 +0300

    Add port option to be set on ssh connection

--- lib/mina/slack/tasks.rb
+++ lib/mina/slack/tasks.rb
@@ -3,12 +3,12 @@ namespace :slack do
   task :post_info do
     if (url = fetch(:slack_url)) && (room = fetch(:slack_room))
       if set?(:user)
-        Net::SSH.start(fetch(:domain), fetch(:user)) do |ssh|
+        Net::SSH.start(fetch(:domain), fetch(:user), port: fetch(:port, 22)) do |ssh|
           set(:last_revision, ssh.exec!("cd #{fetch(:deploy_to)}/scm; git log -n 1 --pretty=format:'%H' #{fetch(:branch)} --"))
         end
       else
         login_data = fetch(:domain).split('@')
-        Net::SSH.start(login_data[1], login_data[0]) do |ssh|
+        Net::SSH.start(login_data[1], login_data[0], port: fetch(:port, 22)) do |ssh|
           set(:last_revision, ssh.exec!("cd #{fetch(:deploy_to)}/scm; git log -n 1 --pretty=format:'%H' #{fetch(:branch)} --"))
         end
       end
